9db5eb451f8889e91dd54f7f76ad002f2dcf2bb0
mirchenari <mohammadmir9691@gmail.com>
2025-10-03 20:27:03 +0330

    Fix spinner issue for links

--- src/components/UI/spinner.jsx
+++ src/components/UI/spinner.jsx
@@ -1,6 +1,10 @@
-export default function Spinner() {
+export default function Spinner({ isMain }) {
   return (
-    <div className="absolute inset-0 flex items-center justify-center z-50 bg-black/40">
+    <div
+      className={`${
+        isMain ? "fixed" : "absolute"
+      } inset-0 flex items-center justify-center z-50 bg-black/40`}
+    >
       <span className="loader"></span>
     </div>
   );
